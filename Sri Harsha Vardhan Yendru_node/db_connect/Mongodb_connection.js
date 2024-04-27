07ddf1f122a9bd60fe130e4e4d8b77d3d433f55f
harsha2001-ai <harshayendru2001@gmail.com>
2024-04-27 18:49:18 -0400

    Project FIles

---
+++ Sri Harsha Vardhan Yendru_node/db_connect/Mongodb_connection.js
@@ -0,0 +1,6 @@
+module.exports = {
+    connectionurl: 'mongodb+srv://Harsha2001:Harsha@cluster0.6bnv11e.mongodb.net/',
+    database: 'Online_Food_Delivery',
+    collection: 'Previous Orders Details'
+  };
+  
